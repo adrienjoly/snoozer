deb9846f8183326bc875a1096277de8de990e027
Adrien Joly <adrien@whyd.com>
2016-10-04 12:53:05 +0200

    added traces to client, and tested with ngrok locally

--- greeter_client.js
+++ greeter_client.js
@@ -33,20 +33,21 @@
 
 var PROTO_PATH = __dirname + '/helloworld.proto';
 
+var HOST = '0.tcp.ngrok.io:18995'; // 'localhost:50051';
+
 var grpc = require('grpc');
 var hello_proto = grpc.load(PROTO_PATH).helloworld;
 
 function main() {
-  var client = new hello_proto.Greeter('localhost:50051',
-                                       grpc.credentials.createInsecure());
-  var user;
-  if (process.argv.length >= 3) {
-    user = process.argv[2];
-  } else {
-    user = 'world';
-  }
-  client.sayHello({name: user}, function(err, response) {
-    console.log('Greeting:', response.message);
+  console.log('host:', HOST);
+  var client = new hello_proto.Greeter(HOST, grpc.credentials.createInsecure());
+  var params = {
+    name: process.argv[2] || 'world'
+  };
+  console.log('calling sayHello with params:', params, '...');
+  client.sayHello(params, function(err, response) {
+    if (err) throw err;
+    console.log('=> Greeting:', response.message);
   });
 }
 

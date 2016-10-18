bb0d644a0bd357c1fa7e1143852742c4239141a8
Adrien Joly <adrien@whyd.com>
2016-10-18 23:00:37 +0200

    updated protobuf API => fixed client

--- tests/client.js
+++ tests/client.js
@@ -1,22 +1,39 @@
 var PROTO_PATH = __dirname + '/../snoozer.proto';
 var HOST = 'localhost:50051'; // or something like '0.tcp.ngrok.io:17802'
 
+var readline = require('readline');
 var grpc = require('grpc');
 var protocol = grpc.load(PROTO_PATH).snoozer;
 
-function main() {
-  console.log('host:', HOST);
-  var client = new protocol.Snoozer(HOST, grpc.credentials.createInsecure());
+console.log('host:', HOST);
+var client = new protocol.Snoozer(HOST, grpc.credentials.createInsecure());
 
+function main(err, sesId) {
   console.log('calling listEvents ...');
-  client.listEvents({}, function(err, response) {
+  client.listEvents({ sesId: sesId }, function(err, response) {
     if (err) throw err;
     console.log('=> events:', response.events);
     var lastEventId = response.events.pop().id;
-    client.swipeEvent({ eventId: lastEventId }, function(err, response) {
+    client.swipeEvent({ sesId: sesId, eventId: lastEventId }, function(err, response) {
       console.log('swiped last event =>', arguments);
     })
   });
 }
 
-main();
+function auth(callback) {
+  var authUrl = client.authToGoogleCalendar({}, function(err, response){
+    console.log('Authorize this app by visiting this url: ', response.url);
+    var rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+    rl.question('Enter the code from that page here: ', function(code) {
+      rl.close();
+      client.getSessionFromCode(code, function(err, response) {
+        callback(err, (response || {}).sesId);
+      });
+    });
+  });
+}
+
+auth(main);

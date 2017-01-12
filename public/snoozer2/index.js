4b756f7274d5b1d476570ec24b6759790cc960e1
Adrien Joly <adrien@whyd.com>
2017-01-12 15:33:22 +0100

    minor js refactor & reordering

--- public/snoozer2/index.js
+++ public/snoozer2/index.js
@@ -1,78 +1,35 @@
-// Your Client ID can be retrieved from your project in the Google
-// Developer Console, https://console.developers.google.com
+// Google API credentials, cf https://console.developers.google.com
 var CLIENT_ID = '607551437730-mikge9rtaacql2b3mb793v8mai34ebg2.apps.googleusercontent.com';
-
 var SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
 
-/**
- * Check if current user has authorized this application.
- */
-function checkAuth() {
-  gapi.auth.authorize(
-    {
-      'client_id': CLIENT_ID,
-      'scope': SCOPES.join(' '),
-      'immediate': true
-    }, handleAuthResult);
-}
+// API Helpers
 
-/**
- * Handle response from authorization server.
- *
- * @param {Object} authResult Authorization result.
- */
-function handleAuthResult(authResult) {
-  var authorizeDiv = document.getElementById('authorize-div');
-  if (authResult && !authResult.error) {
-    // Hide auth UI, then load client library.
-    authorizeDiv.style.display = 'none';
-    loadCalendarApi();
-  } else {
-    // Show auth UI, allowing the user to initiate authorization by
-    // clicking authorize button.
-    authorizeDiv.style.display = 'inline';
-  }
-}
-
-/**
- * Initiate auth flow in response to user clicking authorize button.
- *
- * @param {Event} event Button click event.
- */
-function handleAuthClick(event) {
-  gapi.auth.authorize(
-    {client_id: CLIENT_ID, scope: SCOPES, immediate: false},
-    handleAuthResult);
-  return false;
-}
-
-/**
- * Load Google Calendar client library. List upcoming events
- * once client library is loaded.
- */
-function loadCalendarApi() {
-  gapi.client.load('calendar', 'v3', listUpcomingEvents);
-}
-
-/**
- * Print the summary and start datetime/date of the next ten events in
- * the authorized user's calendar. If no events are found an
- * appropriate message is printed.
- */
-function listUpcomingEvents() {
-  var request = gapi.client.calendar.events.list({
+// fetch the next ten events in the authorized user's calendar.
+function getUpcomingEvents(callback) {
+  gapi.client.calendar.events.list({
     'calendarId': 'primary',
     'timeMin': (new Date()).toISOString(),
     'showDeleted': false,
     'singleEvents': true,
     'maxResults': 10,
     'orderBy': 'startTime'
-  });
+  }).execute(callback);
+}
 
-  request.execute(function(resp) {
+// UI Helpers
+
+// Append a pre element to the body containing the given message
+function appendPre(message) {
+  var pre = document.getElementById('output');
+  var textContent = document.createTextNode(message + '\n');
+  pre.appendChild(textContent);
+}
+
+// Print the next ten events in the authorized user's calendar.
+function printUpcomingEvents() {
+  getUpcomingEvents(function(resp) {
     var events = resp.items;
     appendPre('Upcoming events:');
-
     if (events.length > 0) {
       for (i = 0; i < events.length; i++) {
         var event = events[i];
@@ -85,18 +42,39 @@ function listUpcomingEvents() {
     } else {
       appendPre('No upcoming events found.');
     }
-
   });
 }
 
-/**
- * Append a pre element to the body containing the given message
- * as its text node.
- *
- * @param {string} message Text to be placed in pre element.
- */
-function appendPre(message) {
-  var pre = document.getElementById('output');
-  var textContent = document.createTextNode(message + '\n');
-  pre.appendChild(textContent);
+// Main App / UI logic
+
+// Handle response from authorization server.
+function handleAuthResult(authResult) {
+  var authorizeDiv = document.getElementById('authorize-div');
+  if (authResult && !authResult.error) {
+    // Hide auth UI, then load client library.
+    authorizeDiv.style.display = 'none';
+    gapi.client.load('calendar', 'v3', printUpcomingEvents);
+  } else {
+    // Show auth UI, allowing the user to initiate authorization by
+    // clicking authorize button.
+    authorizeDiv.style.display = 'inline';
+  }
+}
+
+// Initiate auth flow in response to user clicking authorize button.
+function handleAuthClick(event) {
+  gapi.auth.authorize(
+    {client_id: CLIENT_ID, scope: SCOPES, immediate: false},
+    handleAuthResult);
+  return false;
+}
+
+// Check if current user has authorized this application. Run on app startup.
+function checkAuth() {
+  gapi.auth.authorize(
+    {
+      'client_id': CLIENT_ID,
+      'scope': SCOPES.join(' '),
+      'immediate': true
+    }, handleAuthResult);
 }
